4a01fd9d2adf9426cba20d3ada1c67931d346024
rachid elaid <rachidelaid@gmail.com>
2022-03-01 14:50:49 +0100

    fetch missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,7 +1,16 @@
+const SAVE_MISSIONS = 'missionsStore/SAVE_MISSIONS';
+
+export const saveMissions = (payload) => ({
+  type: SAVE_MISSIONS,
+  payload,
+});
+
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case SAVE_MISSIONS:
+      return action.payload;
     default:
       return state;
   }
