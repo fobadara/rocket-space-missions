bdb51706ac0ff5af98d30dad6d3cd7c47bb56c0c
rachid elaid <rachidelaid@gmail.com>
2022-03-03 11:27:55 +0100

    Display joined missions in My profile

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,6 +1,29 @@
-const Profile = () => (
-  <section className="profile">
-    <p>this is the profile page</p>
-  </section>
-);
+import { useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
+import './profile.css';
+
+const Profile = () => {
+  const list = useSelector(({ missionsReducer }) => missionsReducer);
+
+  return (
+    <section className="profile">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list
+            .filter((m) => m.reserved)
+            .map(({ mission_id: id, mission_name: name }) => (
+              <tr key={id}>
+                <td className="name">{name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </section>
+  );
+};
 export default Profile;
